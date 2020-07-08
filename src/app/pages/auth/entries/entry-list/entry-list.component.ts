e2d67f618106fa4b8ecb0e5d3e09c32800b80938
wosiakkk <brunofwosiak@gmail.com>
2020-07-07 23:16:02 -0300

    Estrutura incial para paginacao com filtro

--- src/app/pages/auth/entries/entry-list/entry-list.component.ts
+++ src/app/pages/auth/entries/entry-list/entry-list.component.ts
@@ -1,11 +1,13 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef, ViewChild, ElementRef } from '@angular/core';
 import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 import { Entry } from "../shared/entry.model"
 import { EntryService } from "../shared/entry-service"
 import { TokenStorageService } from 'src/app/security/services/token-storage.service';
 import { ToastMessagesService } from 'src/app/shared/services/toast-messages.service';
 import { faSearch, faFilter } from '@fortawesome/free-solid-svg-icons';
-
+import { LazyLoadEvent } from 'primeng/api';
+import { Pageable } from 'src/app/shared/interfaces/pageable.interface';
+import { User } from 'src/app/security/models/user.model';
 
 @Component({
   selector: 'app-entry-list',
@@ -16,6 +18,9 @@ export class EntryListComponent extends BaseResourceListComponent<Entry> {
 
     faSearch = faSearch;
     faFilter = faFilter;
+
+    @ViewChild('month') month: ElementRef = null;
+    @ViewChild('year') year: ElementRef = null;
 
     cols: any[] =  [
         { field: 'date', header: 'Data', width:'10%' },
@@ -32,6 +37,54 @@ export class EntryListComponent extends BaseResourceListComponent<Entry> {
         private entryService: EntryService 
     ) {
         super(entryService,change,toastService,tokenSotrage);
+    }
+
+    //override
+    loadLazyData(event: LazyLoadEvent) {
+        console.log("event: "+ JSON.stringify(event));
+        this.loading = true;
+        this.change.detectChanges();
+
+        const month = this.month.nativeElement.value;
+        const year = this.year.nativeElement.value;
+
+        const pageableData: Pageable = {
+            page: (event.first / 5),
+            size: event.rows,
+            sort: event.globalFilter
+        }
+
+        setTimeout(() => {
+            let user:User = this.loadAuthResource();
+            if(month != "" && year != ""){
+                this.entryService.getAllPaginationByMonthAndYear(pageableData,month,year,user).subscribe(
+                    resources => {
+                        this.resources = resources;
+                        this.totalRecords = this.entryService.totalElements;
+                        this.loading = false;
+                        if(this.resources.length<=0)
+                            this.emptyList=true;
+                        this.change.detectChanges();
+                    },
+                    error => {
+                        this.toastService.loadServerListErrorToast();
+                    }
+                )
+            }
+            this.entryService.getAllPagination(pageableData,user).subscribe(
+                resources => {
+                    this.resources = resources;
+                    this.totalRecords = this.entryService.totalElements;
+                    this.loading = false;
+                    if(this.resources.length<=0)
+                        this.emptyList=true;
+                    this.change.detectChanges();
+                },
+                error => {
+                    this.toastService.loadServerListErrorToast();
+                }
+            )
+        }, 300 );   
     }
 
 
