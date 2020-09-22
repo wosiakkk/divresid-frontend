0db4bf862503fc7b52cf3902a91ea0671372c7da
wosiakkk <brunofwosiak@gmail.com>
2020-09-21 21:41:16 -0300

    Adicionado estrutura inical de profile de property

---
+++ src/app/pages/auth/properties/property-profile/property-profile.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/security/models/user.model';
+import { TokenStorageService } from 'src/app/security/services/token-storage.service';
+import { ToastMessagesService } from 'src/app/shared/services/toast-messages.service';
+import { Property } from '../shared/property.model';
+import { PropertyService } from '../shared/property.service';
+
+@Component({
+  selector: 'app-property-profile',
+  templateUrl: './property-profile.component.html',
+  styleUrls: ['./property-profile.component.css']
+})
+export class PropertyProfileComponent implements OnInit {
+
+    authUser: User;
+    propertyActive: Property = new Property();
+    onLoading: boolean = false;
+
+    constructor(
+        private propertyService: PropertyService,
+        private toastService: ToastMessagesService,
+        private tokenService: TokenStorageService
+    ) { }
+
+    ngOnInit(): void {
+        this.onLoading = true;
+        this.authUser = this.tokenService.currentUser;
+        this.propertyService
+            .getCurrentActivePropertyId(this.authUser.id).subscribe(
+                property => {
+                    this.onLoading = false;
+                    this.propertyActive = property
+                },
+                error => {
+                    this.onLoading = false;
+                    this.toastService
+                        .loadErrorToast("Problema ao carregar perfil"
+                        + error,
+                        "toast-bottom-center");
+                }
+            )
+    }
+
+}
