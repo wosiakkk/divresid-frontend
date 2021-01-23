d11700874be35169214d5071dc4acad38e1667a7
wosiakkk <brunofwosiak@gmail.com>
2021-01-23 18:17:06 -0300

    Adicionado form para Metas'

--- src/app/pages/auth/goals/goals-form/goals-form.component.ts
+++ src/app/pages/auth/goals/goals-form/goals-form.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { TokenStorageService } from 'src/app/security/services/token-storage.service';
+import { BaseResourceFormComponent } from 'src/app/shared/components/base-resource-form/base-resource-form.component';
+import { ToastMessagesService } from 'src/app/shared/services/toast-messages.service';
+import { Goal } from '../shared/goal.model';
+import { GoalService } from '../shared/goal.service';
 
 @Component({
   selector: 'app-goals-form',
   templateUrl: './goals-form.component.html',
   styleUrls: ['./goals-form.component.css']
 })
-export class GoalsFormComponent implements OnInit {
+export class GoalsFormComponent extends BaseResourceFormComponent<Goal> {
+    
+    
+    protected buildResourceForm(): void {
+        this.resourceForm = this.formBuilder.group({
+            id: [null],
+            name: [null,[Validators.required]],
+            date: [null],
+            value: [null],
+            user: [this.loadAuthResource()]
+        });
+    }
 
-  constructor() { }
+    imaskConfig = {
+        mask: Number,
+        scale: 2,
+        thousandsSeparator: '',
+        padFractionalZeros: true,
+        normalizeZeros: true,
+        radix: ','
+    };
 
-  ngOnInit(): void {
-  }
+    //config calendar primeng
+    ptBR= {
+        firstDayOfWeek: 0,
+        dayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+        dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+        dayNamesMin: ["Do","Se","Te","Qu","Qu","Se","Sa"],
+        monthNames: [ "Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro" ],
+        monthNamesShort: [ "Jan", "Fev", "Mar", "Abr", "Mai", "Jun","Jul", "Ago", "Set", "Out", "Nov", "Dez" ],
+        today: 'Hoje',
+        clear: 'Limpar',
+    }
 
+
+    constructor(
+        protected injector: Injector,
+        protected goalService: GoalService,
+        protected toastService: ToastMessagesService,
+        protected tokenService: TokenStorageService
+    ) { 
+        super(injector, new Goal, goalService, Goal.fromJson
+            ,toastService,tokenService);
+    }
+
+ 
+     //sobrescrevendo os métodos para o título da página, para não utilizar o valor padrão
+     protected creationPageTitle(): string{
+        return "Cadastro de uma nova Meta";
+    }
+
+    protected editionPageTitle(): string{
+        const resourceName = this.resource.name || ""; //caso ainda nao tenha sido carregado o dado, "" será exibido, evitnado o undefined
+        return "Editando Meta: "+ resourceName;
+    }
 }
--- src/app/pages/auth/goals/goals.module.ts
+++ src/app/pages/auth/goals/goals.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core'
 import { GoalsFormComponent } from './goals-form/goals-form.component'
 import { GoalsListComponent } from './goals-list/goals-list.component'
 import { SharedModule } from '../../../shared/shared.module'
-import {TableModule} from 'primeng/table';
-import {PaginatorModule} from 'primeng/paginator';
+import { TableModule } from 'primeng/table';
+import { PaginatorModule } from 'primeng/paginator';
 import { GoalsRoutingModule } from './goals-routing.module';
+import { CalendarModule } from "primeng/calendar";
+import { IMaskModule } from "angular-imask";
 
 @NgModule({
     declarations: [GoalsFormComponent, GoalsListComponent],
-    imports: [GoalsRoutingModule, SharedModule, TableModule,PaginatorModule]
+    imports: [GoalsRoutingModule, SharedModule,
+        TableModule,PaginatorModule, CalendarModule, IMaskModule]
 })
 export class GoalsModule { }
